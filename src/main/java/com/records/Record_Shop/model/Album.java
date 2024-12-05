1023b1c2c9e36c7222ed87d0f8a5c7af10d67f69
nnurhussien04 <nnurhussien03@gmail.com>
2024-12-05 10:05:53 +0000

    have edited the application properites file and created a model package with an album object inside

--- src/main/java/com/records/Record_Shop/model/Album.java
+++ src/main/java/com/records/Record_Shop/model/Album.java
@@ -1,4 +1,41 @@
 package com.records.Record_Shop.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Album {
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @Column
+    String name;
+
+    @Column
+    String artist;
+
+    @Column
+    Integer year;
+
+    @Column
+    Integer price;
+
+    @Column
+    Integer stock;
+
+    @Column
+    Integer sales;
+
+
+
+
 }
