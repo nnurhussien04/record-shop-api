1dc179f686d2a63e5b2831298aad5482cb995526
nnurhussien04 <nnurhussien03@gmail.com>
2024-12-05 09:33:41 +0000

    launching the repo, adding files into it

---
+++ src/main/java/com/records/Record_Shop/RecordShopApplication.java
@@ -0,0 +1,13 @@
+package com.records.Record_Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordShopApplication.class, args);
+	}
+
+}
